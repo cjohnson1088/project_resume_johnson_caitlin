abea70241e46ff4ad7857d19a18ccb7732857799
Caitlin Johnson <caitlin.johnson3130@gmail.com>
2017-07-27 11:37:10 -0400

    adds animated skills bar jQuery plugin

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 $('#responsiveTabs').responsiveTabs({
     startCollapsed: 'accordion'
 });
+
+$('.skillbar').skillBars({
+
+  // number start
+    from: 0,
+
+  // number end
+    to: false,
+
+  // animation speed
+    speed: 3000,
+
+  // how often the element should be up<a href="http://www.jqueryscript.net/time-clock/">date</a>d
+    interval: 100,
+
+  // the number of decimal places to show
+    decimals: 0,
+
+  // callback method for every time the element is updated,
+    onUpdate: null,
+
+  // callback method for when the element finishes updating
+    onComplete: null,
+
+  // CSS classes
+    classes: {
+        skillBarBar: '.skillbar-bar',
+        skillBarPercent: '.skill-bar-percent',
+    }
+});
