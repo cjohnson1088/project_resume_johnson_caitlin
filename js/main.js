5a41044689b67f6d8853adc32eb39d7e94275112
Caitlin Johnson <caitlin.johnson3130@gmail.com>
2017-07-27 23:25:58 -0400

    add jQuery function to change when skillsbars grow

--- js/main.js
+++ js/main.js
@@ -8,32 +8,66 @@ $('.r-tabs').first().on('click', function () {
     $('#my-pic').fadeIn('slow').css('display', 'inline-block');
 });
 
-$('.skillbar').skillBars({
+$('.on-click').on('click', function () {
+    $('.skillbar').skillBars({
 
-  // number start
-    from: 0,
+      // number start
+        from: 0,
 
-  // number end
-    to: false,
+      // number end
+        to: false,
 
-  // animation speed
-    speed: 3000,
+      // animation speed
+        speed: 3000,
 
-  // how often the element should be up<a href="http://www.jqueryscript.net/time-clock/">date</a>d
-    interval: 100,
+      // how often the element should be up<a href="http://www.jqueryscript.net/time-clock/">date</a>d
+        interval: 100,
 
-  // the number of decimal places to show
-    decimals: 0,
+      // the number of decimal places to show
+        decimals: 0,
 
-  // callback method for every time the element is updated,
-    onUpdate: null,
+      // callback method for every time the element is updated,
+        onUpdate: null,
 
-  // callback method for when the element finishes updating
-    onComplete: null,
+      // callback method for when the element finishes updating
+        onComplete: null,
 
-  // CSS classes
-    classes: {
-        skillBarBar: '.skillbar-bar',
-        skillBarPercent: '.skill-bar-percent',
-    }
+      // CSS classes
+        classes: {
+            skillBarBar: '.skillbar-bar',
+            skillBarPercent: '.skill-bar-percent',
+        }
+    });
+});
+
+$('.r-tabs-anchor').on('click', function () {
+    $('.skillbar').skillBars({
+
+      // number start
+        from: 0,
+
+      // number end
+        to: false,
+
+      // animation speed
+        speed: 3000,
+
+      // how often the element should be up<a href="http://www.jqueryscript.net/time-clock/">date</a>d
+        interval: 100,
+
+      // the number of decimal places to show
+        decimals: 0,
+
+      // callback method for every time the element is updated,
+        onUpdate: null,
+
+      // callback method for when the element finishes updating
+        onComplete: null,
+
+      // CSS classes
+        classes: {
+            skillBarBar: '.skillbar-bar',
+            skillBarPercent: '.skill-bar-percent',
+        }
+    });
 });
